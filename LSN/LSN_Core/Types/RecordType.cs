c698a099b33fe99f9cd194fa65b898d2b5482185
Paramecium13 <paramecium13@gmail.com>
2016-01-18 20:39:07 -0500

    Added record types.

---
+++ LSN/LSN_Core/Types/RecordType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSN_Core.Types
+{
+	public class RecordType : LSN_Type
+	{
+
+		private readonly Dictionary<string, LSN_Type> Members;
+		public IReadOnlyDictionary<string, LSN_Type> GetMembers() => Members;
+		public override bool BoolVal { get { return true; } }
+
+		public RecordType(string name, Dictionary<string,LSN_Type> members)
+		{
+			Name = name; Members = members;
+		}
+
+		public override ILSN_Value CreateDefaultValue()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override int GetSize()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ LSN/LSN_Core/Values/RecordValue.cs
@@ -0,0 +1,53 @@
+﻿using LSN_Core.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSN_Core.Values
+{
+	public class RecordValue : LSN_Value
+	{
+		private RecordType _Type;
+
+		private Dictionary<string, ILSN_Value> Values = new Dictionary<string, ILSN_Value>();
+
+		public RecordValue(RecordType type, Dictionary<string,ILSN_Value> values)
+		{
+			Type = type; _Type = type;
+			// Assume 'values' has already been type checked.
+			foreach (var pair in values)
+				Values.Add(pair.Key, pair.Value.Clone());
+		}
+
+
+		public RecordValue(RecordType type)
+		{
+			Type = type; _Type = type;
+			foreach (var pair in _Type.GetMembers())
+				Values.Add(pair.Key, pair.Value.CreateDefaultValue());
+		}
+
+		public override ILSN_Value Clone() => new RecordValue(_Type, Values);
+
+
+		public ILSN_Value GetValue(string name) => Values[name]; 
+		// This should be checked by the reifier.
+
+		public void SetValue(string name, ILSN_Value value)
+		{
+			Values[name] = value;
+		}
+
+		public override int GetSize()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string TranslateUniversal()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
