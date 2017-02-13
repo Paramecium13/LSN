8a80b7cb83fa676121e981c42c6fec0f2bd16c69
Matthew David Hannon <paramecium13@gmail.com>
2017-02-13 11:50:28 -0500

    Created GlobalVariableValue and GlobalVariableValueWatched.

---
+++ LSN/LsnCore/GlobalVariableValue.cs
@@ -0,0 +1,28 @@
+﻿using LsnCore.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LsnCore
+{
+	public class GlobalVariableValue
+	{
+		public readonly string Name;
+
+		protected LsnValue _Value;
+
+		public virtual LsnValue Value
+		{
+			get { return _Value; }
+			set { _Value = value; }
+		}
+
+		public GlobalVariableValue(string name,LsnValue value)
+		{
+			Name = name;_Value = value;
+		}
+
+	}
+}
---
+++ LSN/LsnCore/GlobalVariableValueWatched.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LsnCore
+{
+	[Serializable]
+	public sealed class GlobalVariableValueWatched : GlobalVariableValue
+	{
+		public override LsnValue Value
+		{
+			get { return _Value; }
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+
+
+		public GlobalVariableValueWatched(string name, LsnValue value) : base(name, value){}
+
+
+		public void Watch(object arg)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UnWatch(object arg)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
