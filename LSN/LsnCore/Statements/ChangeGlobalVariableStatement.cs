1d72b31565c50403b8991e60798ca0bc7c872913
Matthew David Hannon <paramecium13@gmail.com>
2017-02-13 11:58:47 -0500

    Added ChangeGlobalVariableStatement.

---
+++ LSN/LsnCore/Statements/ChangeGlobalVariableStatement.cs
@@ -0,0 +1,32 @@
+﻿using LsnCore.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LsnCore.Statements
+{
+	public sealed class ChangeGlobalVariableStatement : Statement
+	{
+		private readonly string GlobalVarName;
+
+		private IExpression Value;
+
+		public ChangeGlobalVariableStatement(string name, IExpression value)
+		{
+			GlobalVarName = name; Value = value;
+		}
+
+		public override InterpretValue Interpret(IInterpreter i)
+		{
+			i.SetGlobalVariable(Value.Eval(i), GlobalVarName);
+			return InterpretValue.Base;
+		}
+
+		public override void Replace(IExpression oldExpr, IExpression newExpr)
+		{
+			if (Value == oldExpr) Value = newExpr;
+		}
+	}
+}
