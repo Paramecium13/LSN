efa73e692645c4bf11d95c0600aa721d2ae197df
Matthew David Hannon <paramecium13@gmail.com>
2017-02-13 11:33:18 -0500

    Added GlobalVariableDef.

---
+++ LSN/LsnCore/GlobalVariableDef.cs
@@ -0,0 +1,31 @@
+﻿using LsnCore.Types;
+using LsnCore.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LsnCore
+{
+	public sealed class GlobalVariableDef
+	{
+		public readonly string Name;
+		public readonly TypeId Type;
+		public readonly LsnValue InitialValue;
+
+		public GlobalVariableDef(string name, TypeId type, LsnValue initialValue)
+		{
+			Name = name; Type = type; InitialValue = initialValue;
+		}
+
+		public GlobalVariableDef(string name, TypeId type):this(name,type,LsnValue.Nil){}
+
+
+		internal GlobalVariableDef(string name,LsnType type, LsnValue initialValue) :this(name,type.Id,initialValue){}
+
+
+		internal GlobalVariableDef(string name, LsnType type) : this(name, type.Id, type.CreateDefaultValue()) { }
+
+	}
+}
