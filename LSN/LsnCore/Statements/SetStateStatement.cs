e03fa65ca588b5c15168f6df909ae405a6431240
Matthew David Hannon <paramecium13@gmail.com>
2017-02-26 14:20:10 -0500

    Added SetStateStatement.

---
+++ LSN/LsnCore/Statements/SetStateStatement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LsnCore.Expressions;
+using LsnCore.Values;
+
+namespace LsnCore.Statements
+{
+	public sealed class SetStateStatement : Statement
+	{
+		private readonly string State;
+
+		public SetStateStatement(string state)
+		{
+			State = state;
+		}
+
+		public override InterpretValue Interpret(IInterpreter i)
+		{
+			(i.GetValue(0).Value as ScriptObject).SetState(State);
+			return InterpretValue.Base;
+		}
+
+		public override void Replace(IExpression oldExpr, IExpression newExpr)
+		{}
+	}
+}
