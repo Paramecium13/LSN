b0ee8b2aced04a57804e6d67a54ee8aac406e874
Matthew David Hannon <paramecium13@gmail.com>
2017-04-05 17:56:22 -0400

    Added EventListener.

---
+++ LSN/LsnCore/Types/EventListener.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LsnCore.Types
+{
+	[Serializable]
+	public sealed class EventListener
+	{
+		private readonly LsnEnvironment Environment;
+		private readonly int StackSize;
+
+		public void Run(LsnValue[] args, IInterpreter i)
+		{
+			i.EnterFunctionScope(Environment, StackSize);
+			for (int argI = 0; argI < args.Length; argI++)
+				i.SetValue(argI, args[argI]);
+		}
+
+	}
+}
