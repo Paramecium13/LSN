03add8c68db3b2bcb7d4a2441f2fef78a1b05e3e
Matthew David Hannon <paramecium13@gmail.com>
2018-01-30 11:45:37 -0500

    Removed script file feature.

--- LSNr/Config.cs
+++ LSNr/Config.cs
@@ -8,7 +8,6 @@ namespace LSNr
 {
 	public class Config
 	{
-
 		//public Dictionary<string, DateTime> LastReifyTimes = new Dictionary<string, DateTime>();
 		public string ObjectFileExtension = ".dat";
 	}
