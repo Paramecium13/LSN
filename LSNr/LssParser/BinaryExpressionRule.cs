12cc6236e3382607ae43b0339b5b2f944fd92990
Paramecium13 <paramecium13@gmail.com>
2018-05-03 21:23:29 -0400

    Added IRule and BinaryExpressionRule. Updated LSNr to C# 7.2 and .Net 4.7.1.

---
+++ LSNr/LssParser/BinaryExpressionRule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LsnCore.Expressions;
+
+namespace LSNr.LssParser
+{
+	public delegate bool ContextCheck(int index, Token[] tokens, IPreScript script, IReadOnlyDictionary<Token, IExpression> substitutions);
+
+	public class BinaryExpressionRule : IRule
+	{
+		public uint Priority { get; private set; }
+		private readonly string operatorValue;
+		private readonly Func<IExpression, IExpression, IExpression> createExpr;
+		private readonly ContextCheck contextCheck;
+
+		public BinaryExpressionRule(uint priority, string opVal, Func<IExpression, IExpression, IExpression> create, ContextCheck cxtCheck = null)
+		{
+			Priority = priority; operatorValue = opVal; createExpr = create; contextCheck = cxtCheck;
+			if (create == null)
+				throw new ArgumentNullException(nameof(create));
+		}
+
+		public bool CheckToken(Token token, IPreScript script)
+			=> token.Value == operatorValue;
+
+		public bool CheckContext(int index, Token[] tokens, IPreScript script, IReadOnlyDictionary<Token, IExpression> substitutions)
+			=> contextCheck?.Invoke(index, tokens, script, substitutions) ?? true;
+
+			public (IExpression expression, int indexOfNextToken, ushort numTokensToRemoveFromLeft)
+			CreateExpression(int index, Token[] tokens, IPreScript script, IReadOnlyDictionary<Token, IExpression> substitutions)
+		{
+			IExpression left, right;
+			if (index == 0) throw new ApplicationException();
+
+			left = null;
+			right = null;
+
+			if (substitutions.ContainsKey(tokens[index - 1]))
+				left = substitutions[tokens[index - 1]];
+			else left = Create.SingleTokenExpress(tokens[index - 1], script);
+
+			if (substitutions.ContainsKey(tokens[index + 1]))
+				right = substitutions[tokens[index - 1]];
+			else right = Create.SingleTokenExpress(tokens[index + 1], script);
+
+			return (createExpr(left, right), index + 2, 1);
+		}
+
+	}
+}
---
+++ LSNr/LssParser/IRule.cs
@@ -0,0 +1,27 @@
+﻿using LsnCore.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSNr.LssParser
+{
+	public interface IRule
+	{
+		uint Priority { get; }
+
+		/// <summary>
+		/// IsInitializingToken...
+		/// </summary>
+		/// <param name="token"></param>
+		/// <param name="script"></param>
+		/// <returns></returns>
+		bool CheckToken(Token token, IPreScript script);
+
+		bool CheckContext(int index, Token[] tokens, IPreScript script, IReadOnlyDictionary<Token, IExpression> substitutions);
+
+		(IExpression expression, int indexOfNextToken, ushort numTokensToRemoveFromLeft)
+			CreateExpression(int index, Token[] tokens, IPreScript script, IReadOnlyDictionary<Token,IExpression> substitutions);
+	}
+}
