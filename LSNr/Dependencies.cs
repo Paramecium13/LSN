3a31163476a514af118854be1aca3d6c12f9746b
Paramecium13 <paramecium13@gmail.com>
2018-09-02 14:51:58 -0400

    Added DependenciesFile.

---
+++ LSNr/Dependencies.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LSNr
+{
+	class DependenciesFile
+	{
+
+		internal readonly Dictionary<string, IReadOnlyList<string>> Dependencies;
+
+		private DependenciesFile(Dictionary<string, IReadOnlyList<string>> deps)
+		{
+			Dependencies = deps;
+		}
+
+		internal void Write(string path)
+		{
+			using (var writer = new StreamWriter(File.Open(path, FileMode.Create)))
+				writer.Write(JsonConvert.SerializeObject(Dependencies, Formatting.Indented));
+		}
+
+		internal static DependenciesFile Read(string path)
+		{
+			var json = (JObject)JToken.Parse(File.ReadAllText(path));
+			var deps = new Dictionary<string, IReadOnlyList<string>>();
+			List<string> ls;
+			foreach (var item in json)
+			{
+				if (item.Value.Type == JTokenType.String)
+					ls = new List<string> { item.Value.Value<string>() };
+				else if (item.Value.Type == JTokenType.Array)
+				{
+					if(!item.Value.All(v => v.Type == JTokenType.String))
+						throw new ApplicationException("Invalid Dependencies File");
+					ls = item.Value.Values<string>().ToList();
+				}
+				else throw new ApplicationException("Invalid Dependencies File");
+
+				deps.Add(item.Key, ls);
+			}
+
+			return new DependenciesFile(deps);
+		}
+
+		internal static DependenciesFile SetUp()
+		{
+			var deps = new Dictionary<string, IReadOnlyList<string>>();
+			Directory.EnumerateFiles("src", "*.lsn", SearchOption.AllDirectories)
+				.AsParallel()
+				.Select(path =>
+				{
+					var usings = new List<string>();
+					using (var sr = new StreamReader(File.OpenRead(path)))
+					{
+						while(!sr.EndOfStream)
+						{
+							var line = sr.ReadLine();
+							if (Regex.IsMatch(line, "#using", RegexOptions.IgnoreCase))
+							{
+								var u = Regex.Match(line, "#using\\s+\"(.+)\"").Groups
+									.OfType<object>()
+									.Select(o => o.ToString())
+									.Skip(1)
+									.First();
+								usings.Add(u);
+							}
+						}
+					}
+					return new KeyValuePair<string, string[]>(path, usings.ToArray());
+				});
+
+			return new DependenciesFile(deps);
+		}
+	}
+}
