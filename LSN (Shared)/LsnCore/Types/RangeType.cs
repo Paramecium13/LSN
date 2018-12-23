4583a56d5bf953ffa1beab458817ff2aa1939017
Paramecium13 <paramecium13@gmail.com>
2018-12-23 13:20:59 -0500

    Added RangeType and RangeValue.

---
+++ LSN (Shared)/LsnCore/Types/RangeType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LsnCore.Types
+{
+	public class RangeType : LsnType
+	{
+		public static readonly RangeType Instance = new RangeType(); 
+		public override LsnValue CreateDefaultValue() => new LsnValue(new Values.RangeValue(0, 0));
+
+		private RangeType()
+		{
+			Name = "IntRange";
+			Id = new TypeId(this);
+		}
+	}
+}
---
+++ LSN (Shared)/LsnCore/Values/RangeValue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LsnCore.Types;
+using Syroot.BinaryData;
+
+namespace LsnCore.Values
+{
+	public class RangeValue : ILsnValue
+	{
+		public readonly int Start;
+		public readonly int End;
+
+		public TypeId Type => Types.RangeType.Instance.Id;
+
+		public bool BoolValue => true;
+
+		public RangeValue(int start, int end)
+		{
+			Start = start; End = end;
+		}
+
+		public ILsnValue Clone() => this;
+
+		public void Serialize(BinaryDataWriter writer)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
