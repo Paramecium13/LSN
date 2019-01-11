14eae9c0a0e956a00c2f7b8ba79171849969d501
Paramecium13 <paramecium13@gmail.com>
2019-01-11 14:51:29 -0500

    Added GoToStatementRule.

--- LSNr/Statements/CommandStatementRules.cs
+++ LSNr/Statements/CommandStatementRules.cs
@@ -54,4 +54,65 @@ namespace LSNr.Statements
 			return new SayStatement(message, graphic, title);
 		}
 	}
+
+	public sealed class GoToStatementRule : IStatementRule
+	{
+		public int Order => throw new NotImplementedException();
+
+		public bool PreCheck(Token t) => true;
+
+		public bool Check(ISlice<Token> tokens, IPreScript script)
+			=> tokens.Any(t => t.Value == "goto");
+
+		public Statement Apply(ISlice<Token> tokens, IPreScript script)
+		{
+			/*var res = LssParser.ExpressionParser.MultiParse(tokens, script);
+			tokens = res.tokens.ToSlice();
+			var subs = res.substitutions;
+			if(tokens[0].Type == TokenType.Substitution)
+			...*/
+			IExpression actor = null;
+			IReadOnlyList<Token> tokens0 = null;
+			if (tokens[0].Value == "goto")
+				tokens0 = tokens;
+			else
+			{
+				var actorTokens = tokens.TakeWhile(t => t.Value != "goto").ToList();
+				tokens0 = tokens.Skip(actorTokens.Count + 1).ToList();
+				actor = Create.Express(actorTokens, script);
+			}
+
+			var metaCommaCount = tokens0.Count(t => t.Value == "`");
+			IExpression expr0 = null;
+			IExpression expr1 = null;
+			IExpression expr2 = null;
+			switch (metaCommaCount)
+			{
+				case 0:
+					expr0 = Create.Express(tokens0, script);
+					break;
+				case 1:
+					{
+						var tokens1 = tokens0.TakeWhile(t => t.Value != "`").ToList();
+						var tokens2 = tokens0.Skip(tokens1.Count + 1).ToList();
+						expr0 = Create.Express(tokens1, script);
+						expr1 = Create.Express(tokens2, script);
+						break;
+					}
+				case 2:
+					{
+						var tokens1 = tokens0.TakeWhile(t => t.Value != "`").ToList();
+						var tokens2 = tokens0.Skip(tokens1.Count + 1).TakeWhile(t => t.Value != "`").ToList();
+						var tokens3 = tokens0.Skip(tokens1.Count + tokens2.Count + 2).ToList();
+						expr0 = Create.Express(tokens1, script);
+						expr1 = Create.Express(tokens2, script);
+						expr2 = Create.Express(tokens3, script);
+						break;
+					}
+				default:
+					throw new LsnrParsingException(tokens.First(t => t.Value.ToLower() == "goto"), "Improperly formatted goto statement (considered harmful).", script.Path);
+			}
+			return new GoToStatement(expr0, expr1, expr2, actor);
+		}
+	}
 }
--- LSNr/Statements/IStatementRule.cs
+++ LSNr/Statements/IStatementRule.cs
@@ -1,5 +1,7 @@
-﻿using LsnCore.Statements;
+﻿using LsnCore.Expressions;
+using LsnCore.Statements;
 using LsnCore.Utilities;
+using System.Collections.Generic;
 
 namespace LSNr.Statements
 {
@@ -9,6 +11,13 @@ namespace LSNr.Statements
 		bool PreCheck(Token t);
 		bool Check(ISlice<Token> tokens, IPreScript script);
 		Statement Apply(ISlice<Token> tokens, IPreScript script);
+	}
+
+	public interface ISemiParsedStatementRule
+	{
+		int Order { get; }
+		bool Check(ISlice<Token> tokens, IReadOnlyDictionary<Token, IExpression> substitutions, IPreScript script);
+		Statement Apply(ISlice<Token> tokens, IReadOnlyDictionary<Token, IExpression> substitutions, IPreScript script);
 	}
 
 	public static class StatementRuleOrders
