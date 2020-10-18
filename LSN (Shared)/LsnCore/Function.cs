fb43927757cb5380a6803766d6bbff42aabe9971
Paramecium13 <paramecium13@gmail.com>
2020-10-18 14:25:58 -0400

    Update documentation.

--- LSN (Shared)/LsnCore/Function.cs
+++ LSN (Shared)/LsnCore/Function.cs
@@ -15,22 +15,44 @@ namespace LsnCore
 	/// </summary>
 	public abstract class Function
 	{
+		/// <summary>
+		/// The function's signature
+		/// </summary>
 		public readonly FunctionSignature Signature;
 
+		/// <summary>
+		/// Gets the function's name.
+		/// </summary>
 		public string Name => Signature.Name;
 
+		/// <summary>
+		/// Gets the function's return type, or null if it doesn't return anything (aka returns '()' or <see langword="void"/>).
+		/// </summary>
 		public TypeId ReturnType => Signature.ReturnType;
 
+		/// <summary>
+		/// Gets the function's parameters.
+		/// </summary>
 		public IReadOnlyList<Parameter> Parameters => Signature.Parameters;
 
+		/// <summary>
+		/// Gets or sets the size of the function stack for this function.
+		/// </summary>
 		public int StackSize { get; set; } = -1; // Should only be set in LSNr.
-											// If the value is -1, this indicates that the stack size was never set.
+												 // If the value is -1, this indicates that the stack size was never set.
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Function"/> class.
+		/// </summary>
+		/// <param name="signature">The signature.</param>
 		protected Function(FunctionSignature signature)
 		{
 			Signature = signature;
 		}
 
+		/// <summary>
+		/// The resource file path
+		/// </summary>
 		protected string _ResourceFilePath;
 		public string ResourceFilePath { get => _ResourceFilePath;
 			protected set => _ResourceFilePath = value;
@@ -46,11 +68,33 @@ namespace LsnCore
 	/// </summary>
 	public class Parameter : IEquatable<Parameter>
 	{
+		/// <summary>
+		/// The name of the parameter.
+		/// </summary>
 		public readonly string Name;
+
+		/// <summary>
+		/// The type of the parameter.
+		/// </summary>
 		public readonly TypeId Type;
+
+		/// <summary>
+		/// The parameter's default value. If it doesn't have a default value, <see cref="LsnValue.Nil"/>.
+		/// </summary>
 		public readonly LsnValue DefaultValue;
+
+		/// <summary>
+		/// The parameter's index.
+		/// </summary>
 		public readonly ushort Index;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Parameter"/> class.
+		/// </summary>
+		/// <param name="name">The name of the parameter.</param>
+		/// <param name="type">The type of the parameter.</param>
+		/// <param name="val">The default value of the parameter, or <see cref="LsnValue.Nil"/>.</param>
+		/// <param name="i">The index of the parameter.</param>
 		public Parameter(string name, TypeId type, LsnValue val, ushort i)
 		{
 			Name = name;
@@ -59,6 +103,13 @@ namespace LsnCore
 			Index = i;
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Parameter"/> class.
+		/// </summary>
+		/// <param name="name">The name of the parameter.</param>
+		/// <param name="type">The type of the parameter.</param>
+		/// <param name="val">The default value of the parameter, or <see cref="LsnValue.Nil"/>.</param>
+		/// <param name="i">The index of the parameter.</param>
 		public Parameter(string name, LsnType type, LsnValue val, ushort i)
 		{
 			Name = name;
@@ -67,9 +118,15 @@ namespace LsnCore
 			Index = i;
 		}
 
+		/// <inheritdoc/>
 		public bool Equals(Parameter other)
 			=> Name == other.Name && Type == other.Type && DefaultValue.Equals(other.DefaultValue) && Index == other.Index;
 
+		/// <summary>
+		/// Serializes the specified writer.
+		/// </summary>
+		/// <param name="writer">The writer.</param>
+		/// <param name="resourceSerializer">The resource serializer.</param>
 		public void Serialize(BinaryDataWriter writer, ResourceSerializer resourceSerializer)
 		{
 			writer.Write(Name);
@@ -77,6 +134,13 @@ namespace LsnCore
 			writer.Write(!DefaultValue.IsNull);
 		}
 
+		/// <summary>
+		/// Reads a parameter.
+		/// </summary>
+		/// <param name="index">The <see cref="Index"/> of the parameter. This is determined by its order.</param>
+		/// <param name="reader">The reader.</param>
+		/// <param name="typeContainer">The type container.</param>
+		/// <returns></returns>
 		public static Parameter Read(ushort index, BinaryDataReader reader, ITypeIdContainer typeContainer)
 		{
 			var name = reader.ReadString();
@@ -87,6 +151,13 @@ namespace LsnCore
 			return new Parameter(name, typeId, LsnValue.Nil, index);
 		}
 
+		/// <summary>
+		/// Reads a parameter.
+		/// </summary>
+		/// <param name="index">The <see cref="Index"/> of the parameter. This is determined by its order.</param>
+		/// <param name="reader">The reader.</param>
+		/// <param name="resourceDeserializer">The resource deserializer.</param>
+		/// <returns></returns>
 		public static Parameter Read(ushort index, BinaryDataReader reader, ResourceDeserializer resourceDeserializer)
 		{
 			var name = reader.ReadString();
--- LSNr/LsnCore/InstructionMappedMethod.cs
+++ LSNr/LsnCore/InstructionMappedMethod.cs
@@ -9,16 +9,38 @@ namespace LsnCore
 {
 	public sealed class InstructionMappedMethod : Method
 	{
+		/// <summary>
+		/// The <see cref="LsnCore.OpCode"/> for the <see cref="Instruction"/> that implements this method.
+		/// </summary>
 		public OpCode OpCode { get; }
 
+		/// <summary>
+		/// The data for the <see cref="Instruction"/> that implements this method.
+		/// </summary>
 		public ushort Data { get; }
 
-		/// <inheritdoc />
+		/// <summary>
+		/// Initializes a new instance of the <see cref="InstructionMappedMethod"/> class.
+		/// </summary>
+		/// <param name="type">The type this method belongs to.</param>
+		/// <param name="returnType">The method's return type.</param>
+		/// <param name="name">The name.</param>
+		/// <param name="opCode">The <see cref="LsnCore.OpCode"/>.</param>
+		/// <param name="parameters">The method's parameters.</param>
+		/// <param name="data">The data for this method's <see cref="Instruction"/>.</param>
 		public InstructionMappedMethod(LsnType type, LsnType returnType, string name, OpCode opCode,
 			IReadOnlyList<Parameter> parameters = null, ushort data = 0) : this(type.Id, returnType.Id, name, opCode,
 			parameters, data) {}
 
-		/// <inheritdoc />
+		/// <summary>
+		/// Initializes a new instance of the <see cref="InstructionMappedMethod"/> class.
+		/// </summary>
+		/// <param name="type">The type this method belongs to.</param>
+		/// <param name="returnType">The method's return type.</param>
+		/// <param name="name">The name.</param>
+		/// <param name="opCode">The <see cref="LsnCore.OpCode"/>.</param>
+		/// <param name="parameters">The method's parameters.</param>
+		/// <param name="data">The data for this method's <see cref="Instruction"/>.</param>
 		public InstructionMappedMethod(TypeId type, TypeId returnType, string name, OpCode opCode,
 			IReadOnlyList<Parameter> parameters = null, ushort data = 0)
 			: base(type, returnType, name, parameters ?? new List<Parameter> {new Parameter("self", type, LsnValue.Nil, 0)})
@@ -28,17 +50,43 @@ namespace LsnCore
 		}
 	}
 
+	/// <summary>
+	/// A <see cref="Function"/> that is implemented as a single <see cref="Instruction"/>.
+	/// </summary>
+	/// <seealso cref="LsnCore.Function" />
 	public sealed class InstructionMappedFunction : Function
 	{
+		/// <summary>
+		/// The <see cref="LsnCore.OpCode"/> for the instruction.
+		/// </summary>
 		public OpCode OpCode { get; }
 
+		/// <summary>
+		/// The data for the instruction.
+		/// </summary>
 		public ushort Data { get; }
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="InstructionMappedFunction"/> class.
+		/// </summary>
+		/// <param name="parameters">The parameters.</param>
+		/// <param name="returnType">The function's return type.</param>
+		/// <param name="name">The name of the function.</param>
+		/// <param name="opCode">The <see cref="LsnCore.OpCode"/> for the instruction.</param>
+		/// <param name="data">The data for the instruction.</param>
 		public InstructionMappedFunction(IReadOnlyList<Parameter> parameters, LsnType returnType, string name,
 			OpCode opCode, ushort data = 0)
 			: this(parameters, returnType?.Id, name, opCode, data)
 		{}
-		
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="InstructionMappedFunction"/> class.
+		/// </summary>
+		/// <param name="parameters">The parameters.</param>
+		/// <param name="returnType">The function's return type.</param>
+		/// <param name="name">The name of the function.</param>
+		/// <param name="opCode">The <see cref="LsnCore.OpCode"/> for the instruction.</param>
+		/// <param name="data">The data for the instruction.</param>
 		public InstructionMappedFunction(IReadOnlyList<Parameter> parameters, TypeId returnType, string name,
 			OpCode opCode, ushort data = 0)
 			: base(new FunctionSignature(parameters, name, returnType))
@@ -48,11 +96,33 @@ namespace LsnCore
 		}
 	}
 
+	/// <summary>
+	/// A <see cref="Function"/> that is implemented by multiple instructions.
+	/// </summary>
+	/// <seealso cref="LsnCore.Function" />
 	public sealed class MultiInstructionMappedFunction : Function
 	{
+		/// <summary>
+		/// Gets the <see cref="OpCode"/>s and data for the instructions.
+		/// The instructions are split into a number of sets equal to the number of parameters that this function takes.
+		/// <para/>
+		/// Each set of instructions is inserted after the instructions for the corresponding parameter.
+		/// </summary>
 		public (OpCode Opcode, ushort Data)[][] Instructions { get; }
 
-		/// <inheritdoc />
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MultiInstructionMappedFunction"/> class.
+		/// </summary>
+		/// <param name="parameters">The parameters.</param>
+		/// <param name="returnType">Type of the return.</param>
+		/// <param name="name">The name.</param>
+		/// <param name="instructionSets">
+		///		The <see cref="OpCode"/>s and data for the instructions.
+		///		The instructions are split into a number of sets equal to the number of parameters that this function takes.
+		///		<para/>
+		///		Each set of instructions is inserted after the instructions for the corresponding parameter.
+		/// </param>
+		/// <exception cref="ArgumentException">Bad format... - instructionSets</exception>
 		public MultiInstructionMappedFunction(IReadOnlyList<Parameter> parameters, LsnType returnType, string name,
 			(OpCode opCode, ushort data)[][] instructionSets)
 			: base(new FunctionSignature(parameters, name, returnType?.Id))
